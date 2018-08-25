94b281502e4c5053b36bfa5b5a19a5be454aaad8
Harsh Shandilya <harsh@prjkt.io>
2018-08-25 23:18:35 +0530

    Fix global exclusions retrieval on subsequent launches
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- app/src/main/java/com/wireguard/android/model/GlobalExclusions.kt
+++ app/src/main/java/com/wireguard/android/model/GlobalExclusions.kt
@@ -3,11 +3,8 @@ package com.wireguard.android.model
 import com.wireguard.android.Application
 
 class GlobalExclusions {
-    init {
-        exclusions = Application.getSharedPreferences().getString("global_exclusions", "") as String
-    }
     companion object {
-        var exclusions: String = ""
+        var exclusions: String = Application.getSharedPreferences().getString("global_exclusions", "") as String
         set(value) {
             Application.getSharedPreferences().edit().putString("global_exclusions", value).apply()
             field = value
