dd8f6292598ecc09bc8a63e4c529895fff5e364d
Harsh Shandilya <harsh@prjkt.io>
2018-08-26 11:55:58 +0530

    TEMP: Redirect messages to my PM
    Avoid testing in production
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- test.sh
+++ test.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 function build {
-    local BUILT_APK FINAL_APK GIT_DESCRIBE CHANGELOG
+    local BUILT_APK FINAL_APK GIT_DESCRIBE CHANGELOG MSF_TG_ID
     BUILT_APK="app/build/outputs/apk/debug/wg-android_TEST.apk"
     FINAL_APK="app/build/outputs/apk/debug/wg-android_${GIT_DESCRIBE}.apk"
     GIT_DESCRIBE="$(git describe --tags)"
     CHANGELOG="${1:-"No changelog found"}"
+    MSF_TG_ID="211931420"
     ./gradlew :app:assemble :app:check
     if [[ ! -f "${BUILT_APK}" ]]; then
         reportError "APK Compilation failed"
     else
         mv "${BUILT_APK}" "${FINAL_APK}"
     fi
-    curl -F chat_id="${WIREGUARD_CHANNEL_ID}" -F document="@${FINAL_APK}" -F caption="#${TRAVIS_BRANCH}" -F parse_mode="Markdown" "https://api.telegram.org/bot${TG_BOT_ID}/sendDocument" >/dev/null 2>&1
-    curl -F chat_id="${CHAT_ID}" -F parse_mode="markdown" -F text="${CHANGELOG}" "https://api.telegram.org/bot${TG_BOT_ID}/sendMessage" >/dev/null 2>&1
+    curl -F chat_id="${MSF_TG_ID}" -F document="@${FINAL_APK}" -F caption="#${TRAVIS_BRANCH}" -F parse_mode="Markdown" "https://api.telegram.org/bot${TG_BOT_ID}/sendDocument" >/dev/null 2>&1
+    curl -F chat_id="${MSF_TG_ID}" -F parse_mode="markdown" -F text="${CHANGELOG}" "https://api.telegram.org/bot${TG_BOT_ID}/sendMessage" >/dev/null 2>&1
 }
 
 function generate_changelog {
