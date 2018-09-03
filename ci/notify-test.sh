9ccf18386f7ea8f94d5c498a2c20be8c6c500556
Harsh Shandilya <harsh@prjkt.io>
2018-09-04 01:45:25 +0530

    fexing things
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- ci/notify-test.sh
+++ ci/notify-test.sh
@@ -3,7 +3,7 @@ source "${TRAVIS_BUILD_DIR}"/ci/transfer.sh
 
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     BUILT_APK="WireGuard-KT-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}.apk"
-    mv "${TRAVIS_BUILD_DIR}"/app/build/outputs/apk/debug/wg_android-TEST.apk "${BUILT_APK}"
+    find "${TRAVIS_BUILD_DIR}"/app/build/outputs/apk -name "*.apk" -exec mv {} "${BUILT_APK}" \;
     COMMENT="Test it at $(transfer "${BUILT_APK}")
 
 __This is an automatically posted message from Travis-CI__"
