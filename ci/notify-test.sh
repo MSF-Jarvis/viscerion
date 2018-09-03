7b98f43b4f06e0aebb66de1a63ad82cb58b0223d
Harsh Shandilya <harsh@prjkt.io>
2018-09-03 23:43:42 +0530

    ci: Send APKs back to the PR thread for testers
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

---
+++ ci/notify-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source "${TRAVIS_BUILD_DIR}"/ci/transfer.sh
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    BUILT_APK="$(WireGuard-KT-"${TRAVIS_PULL_REQUEST}"-"${TRAVIS_COMMIT}".apk)"
+    find "${TRAVIS_BUILD_DIR}"/app/build/outputs/apk/debug/ -name *.apk -exec mv {} "${BUILT_APK}"
+    COMMENT="Test it at "$(transfer "${BUILT_APK}")
+
+__This is an automatically posted message from Travis-CI__"
+    curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
