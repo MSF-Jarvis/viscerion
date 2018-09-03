0a0921cd80d95e8b4b7869f5b10d45fbff558121
Harsh Shandilya <harsh@prjkt.io>
2018-09-04 01:28:52 +0530

    FOR FUCK'S SAKE
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- ci/notify-test.sh
+++ ci/notify-test.sh
@@ -2,10 +2,10 @@
 source "${TRAVIS_BUILD_DIR}"/ci/transfer.sh
 
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-    BUILT_APK="WireGuard-KT-"${TRAVIS_PULL_REQUEST}"-"${TRAVIS_COMMIT}".apk"
+    BUILT_APK="WireGuard-KT-${TRAVIS_PULL_REQUEST}-${TRAVIS_COMMIT}.apk"
     mv "${TRAVIS_BUILD_DIR}"/app/build/outputs/apk/debug/wg_android-TEST.apk "${BUILT_APK}"
-    COMMENT="Test it at "$(transfer "${BUILT_APK}")"
+    COMMENT="Test it at $(transfer "${BUILT_APK}")
 
 __This is an automatically posted message from Travis-CI__"
-    curl -H "Authorization: token "${GITHUB_TOKEN}"" -X POST -d "\{\"body\": \"${COMMENT}\"\}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments""
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d "\{\"body\": \"${COMMENT}\"\}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
