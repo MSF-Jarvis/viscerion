MSF-Jarvis/viscerion

commit f50757d6411e3c407f3f773caa1b750b9bba6a68

Extensions: Account for TextInputEditText migration
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/com/wireguard/android/util/Extensions.kt
@@ -16,9 +16,9 @@ import android.os.SystemClock
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
 import android.view.View
-import android.widget.TextView
 import androidx.preference.Preference
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 import com.wireguard.android.activity.SettingsActivity
 import com.wireguard.config.Attribute
 
@@ -78,13 +78,13 @@ fun Context.resolveAttribute(attr: Int): Int {
 }
 
 fun copyTextView(view: View) {
-    if (view !is TextView)
+    if (view !is TextInputEditText)
         return
-    val text = view.text
+    val text = view.editableText
     if (text == null || text.isEmpty())
         return
     val service = view.getContext().getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager ?: return
-    val description = view.getContentDescription()
+    val description = view.hint
     service.primaryClip = ClipData.newPlainText(description, text)
     Snackbar.make(view, description.toString() + " copied to clipboard", Snackbar.LENGTH_LONG).show()
 }

commit c8a06beb4f8cc0b84e4da378509ac471433f8458

Extensions: Re-add support for TextViews in copyTextView
Fixes: f50757d6411e ("Extensions: Account for TextInputEditText migration")
Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

app/src/main/java/com/wireguard/android/util/Extensions.kt
@@ -17,6 +17,7 @@ import android.text.TextUtils
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
 import android.view.View
+import android.widget.TextView
 import androidx.preference.Preference
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
@@ -90,13 +91,16 @@ fun Context.resolveAttribute(attr: Int): Int {
 }
 
 fun copyTextView(view: View) {
-    if (view !is TextInputEditText)
+    var isTextInput = false
+    if (view is TextInputEditText)
+        isTextInput = true
+    else if (view !is TextView)
         return
-    val text = view.editableText
+    val text = if (isTextInput) (view as TextInputEditText).editableText else (view as TextView).text
     if (text == null || text.isEmpty())
         return
-    val service = view.getContext().getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager ?: return
-    val description = view.hint
+    val service = view.context.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager ?: return
+    val description = if (isTextInput) (view as TextInputEditText).hint else view.contentDescription
     service.primaryClip = ClipData.newPlainText(description, text)
     Snackbar.make(view, description.toString() + " copied to clipboard", Snackbar.LENGTH_LONG).show()
 }